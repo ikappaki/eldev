06aa2cc593c8a5ee037eb235dac3ebcda77db41b
ikappaki <ikappaki@users.noreply.github.com>
2021-03-27 12:25:37 +0000

    debug

--- bin/eldev.bat
+++ bin/eldev.bat
@@ -38,6 +38,7 @@ REM the newline variable above MUST be followed by two empty lines.
       ;; This is intentional.  First, this is in case ELDEV_LOCAL is !NL!^
       ;; defined, second, this is just Eldev default for packages. !NL!^
       (load-prefer-newer    t)) !NL!^
+  (message """:staring...""") !NL!^
   ;; Setting `debug-on-error' would be useful, but it can break many !NL!^
   ;; `package-*' functions, since those use `with-demoted-errors' and !NL!^
   ;; so `condition-case-unless-debug'. !NL!^
@@ -48,44 +49,45 @@ REM the newline variable above MUST be followed by two empty lines.
         package-archives       nil) !NL!^
   (require 'package) !NL!^
   (package-initialize t) !NL!^
-  (let ((package-archives '(("""melpa-stable""" . """http://stable.melpa.org/packages/"""))) !NL!^
-        (archive-name      """MELPA Stable""") !NL!^
-        (inhibit-message  t) !NL!^
-        (eldev-local      (getenv """ELDEV_LOCAL""")) !NL!^
-        eldev-pkg !NL!^
-        requirements) !NL!^
-    (unless (= (length eldev-local) 0) !NL!^
-      (if (string-prefix-p """:pa:""" eldev-local) !NL!^
-          (setf package-archives `(("""bootstrap-pa""" . ,(file-name-as-directory (substring eldev-local (length """:pa:"""))))) !NL!^
-                archive-name     """a local package archive""") !NL!^
-        (with-temp-buffer !NL!^
-          (insert-file-contents-literally (expand-file-name """eldev.el""" eldev-local)) !NL!^
-          (setf eldev-pkg                    (package-buffer-info) !NL!^
-                (package-desc-dir eldev-pkg) (expand-file-name eldev-local)) !NL!^
-          ;; Currently Eldev has no external dependencies, but let's be generic. !NL!^
-          (dolist (requirement (package-desc-reqs eldev-pkg)) !NL!^
-            (unless (package-activate (car requirement)) !NL!^
-              (push requirement requirements)))))) !NL!^
-    (when (if eldev-pkg !NL!^
-              requirements !NL!^
-            (not (package-activate 'eldev))) !NL!^
-      (let ((inhibit-message nil)) !NL!^
-        (message """Bootstrapping Eldev for Emacs %%s from %%s...\n""" eldev--emacs-version archive-name) !NL!^
-        (when eldev-pkg !NL!^
-          (message """Eldev package itself will be used from `%%s'\n""" eldev-local))) !NL!^
-      (package-refresh-contents) !NL!^
-      (if eldev-pkg !NL!^
-          (package-download-transaction (package-compute-transaction nil requirements)) !NL!^
-        (package-install 'eldev))) !NL!^
-    (when eldev-pkg !NL!^
-      (push `(eldev . (,eldev-pkg)) package-alist) !NL!^
-      ;; `package--autoloads-file-name' is package-private. !NL!^
-      (let* ((autoloads-file     (expand-file-name (format """%%s-autoloads""" (package-desc-name eldev-pkg)) !NL!^
-                                                   (package-desc-dir eldev-pkg))) !NL!^
-             (autoloads-disabler (lambda (do-load file ^&rest args) (unless (equal file autoloads-file) (apply do-load file args))))) !NL!^
-        (advice-add #'load :around autoloads-disabler) !NL!^
-        (package-activate-1 eldev-pkg) !NL!^
-        (advice-remove #'load autoloads-disabler)))) !NL!^
+  (message """:done...""") !NL!^
+  ;; (let ((package-archives '(("""melpa-stable""" . """http://stable.melpa.org/packages/"""))) !NL!^
+  ;;       (archive-name      """MELPA Stable""") !NL!^
+  ;;       (inhibit-message  t) !NL!^
+  ;;       (eldev-local      (getenv """ELDEV_LOCAL""")) !NL!^
+  ;;       eldev-pkg !NL!^
+  ;;       requirements) !NL!^
+  ;;   (unless (= (length eldev-local) 0) !NL!^
+  ;;     (if (string-prefix-p """:pa:""" eldev-local) !NL!^
+  ;;         (setf package-archives `(("""bootstrap-pa""" . ,(file-name-as-directory (substring eldev-local (length """:pa:"""))))) !NL!^
+  ;;               archive-name     """a local package archive""") !NL!^
+  ;;       (with-temp-buffer !NL!^
+  ;;         (insert-file-contents-literally (expand-file-name """eldev.el""" eldev-local)) !NL!^
+  ;;         (setf eldev-pkg                    (package-buffer-info) !NL!^
+  ;;               (package-desc-dir eldev-pkg) (expand-file-name eldev-local)) !NL!^
+  ;;         ;; Currently Eldev has no external dependencies, but let's be generic. !NL!^
+  ;;         (dolist (requirement (package-desc-reqs eldev-pkg)) !NL!^
+  ;;           (unless (package-activate (car requirement)) !NL!^
+  ;;             (push requirement requirements)))))) !NL!^
+  ;;   (when (if eldev-pkg !NL!^
+  ;;             requirements !NL!^
+  ;;           (not (package-activate 'eldev))) !NL!^
+  ;;     (let ((inhibit-message nil)) !NL!^
+  ;;       (message """Bootstrapping Eldev for Emacs %%s from %%s...\n""" eldev--emacs-version archive-name) !NL!^
+  ;;       (when eldev-pkg !NL!^
+  ;;         (message """Eldev package itself will be used from `%%s'\n""" eldev-local))) !NL!^
+  ;;     (package-refresh-contents) !NL!^
+  ;;     (if eldev-pkg !NL!^
+  ;;         (package-download-transaction (package-compute-transaction nil requirements)) !NL!^
+  ;;       (package-install 'eldev))) !NL!^
+  ;;   (when eldev-pkg !NL!^
+  ;;     (push `(eldev . (,eldev-pkg)) package-alist) !NL!^
+  ;;     ;; `package--autoloads-file-name' is package-private. !NL!^
+  ;;     (let* ((autoloads-file     (expand-file-name (format """%%s-autoloads""" (package-desc-name eldev-pkg)) !NL!^
+  ;;                                                  (package-desc-dir eldev-pkg))) !NL!^
+  ;;            (autoloads-disabler (lambda (do-load file ^&rest args) (unless (equal file autoloads-file) (apply do-load file args))))) !NL!^
+  ;;       (advice-add #'load :around autoloads-disabler) !NL!^
+  ;;       (package-activate-1 eldev-pkg) !NL!^
+  ;;       (advice-remove #'load autoloads-disabler)))) !NL!^
   (require 'eldev) !NL!^
   (eldev-start-up)) !NL!^
 "  ^
